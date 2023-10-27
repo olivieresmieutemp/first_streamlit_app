5250e746ecbf436cd43e209fec15fad06f30b8de
olivieresmieutemp <149180068+olivieresmieutemp@users.noreply.github.com>
2023-10-27 17:16:10 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Bueberry Oatmeal')
-# streamlit.text('BKale, Sinach & Rocket Smoothie')
-# streamlit.text('Hard boiled Free-Range egg')
+streamlit.text('BKale, Sinach & Rocket Smoothie')
+streamlit.text('Hard boiled Free-Range egg')
